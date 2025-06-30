e2a0d5ea4fd711d5f7e239775d5fd8e1411e9ced
sravanthi <sravanthimanchikatla06@gmail.com>
2025-06-30 10:51:34 +0530

    initial commit

---
+++ src/main/java/com/Ecommerce/Nikee/EcommerceNikee01RegistrationLoginApplication.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.Nikee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceNikee01RegistrationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceNikee01RegistrationLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ecommerce/Nikee/EcommerceNikee01RegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.Nikee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceNikee01RegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
